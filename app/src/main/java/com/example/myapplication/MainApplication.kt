cdd97ef324b4c374e66e205b2ee3ac538e29aa0b
DESKTOP-NEO0231\Michael <michaelhsieh0204@gmail.com>
2020-12-09 09:31:50 +0900

    timberとmainapplicationの追加

--- app/src/main/java/com/example/myapplication/MainApplication.kt
+++ app/src/main/java/com/example/myapplication/MainApplication.kt
@@ -1,4 +1,14 @@
 package com.example.myapplication
 
-class MainApplication {
+import android.app.Application
+import timber.log.Timber
+
+class MainApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        // Timberを有効にします
+        if(BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
