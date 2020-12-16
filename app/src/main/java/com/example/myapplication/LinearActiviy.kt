89a8988787f2ab7ba6e934ccbe8bd1d082877285
DESKTOP-NEO0231\Michael <michaelhsieh0204@gmail.com>
2020-12-16 09:54:22 +0900

    Linearlayout infalter追加

--- app/src/main/java/com/example/myapplication/LinearActiviy.kt
+++ app/src/main/java/com/example/myapplication/LinearActiviy.kt
@@ -1,11 +1,21 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_linear_activiy.*
+import kotlinx.android.synthetic.main.item_linearlayout.view.*
 
 class LinearActiviy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.test_commute)
+        setContentView(R.layout.activity_linear_activiy)
+        linearInflater.setOnClickListener {
+            val inflater = LayoutInflater.from(this)
+            val view = inflater.inflate(R.layout.item_linearlayout, linearlayout, false)
+            view.nameid.text = "シュウ"
+            linearlayout.addView(view)
+            
+        }
     }
 }
